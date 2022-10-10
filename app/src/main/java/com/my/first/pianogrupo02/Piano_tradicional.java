3d6b0715df6e16d6247db852d382aa17210ac33e
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-10 01:09:52 -0600

    piano_tradicional_layout

---
+++ app/src/main/java/com/my/first/pianogrupo02/Piano_tradicional.java
@@ -0,0 +1,14 @@
+package com.my.first.pianogrupo02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Piano_tradicional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piano_tradicional);
+    }
+}
