09149204d3e94ea303e79dc39f9c1977da8eaa9a
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-10 02:03:41 -0600

    piano_infantil_layout_1

---
+++ app/src/main/java/com/my/first/pianogrupo02/Piano_infantil_selva.java
@@ -0,0 +1,14 @@
+package com.my.first.pianogrupo02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Piano_infantil_selva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piano_infantil_selva);
+    }
+}
