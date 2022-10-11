0efb66aa528ac8fc5eacf7027bb995c0f248f0f4
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-11 01:52:47 -0600

    menu

--- app/src/main/java/com/my/first/pianogrupo02/AcercaDe.java
+++ app/src/main/java/com/my/first/pianogrupo02/AcercaDe.java
@@ -1,8 +1,15 @@
 package com.my.first.pianogrupo02;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AcercaDe extends AppCompatActivity {
 
@@ -10,5 +17,57 @@ public class AcercaDe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void CerrarAplicacion() {
+        finishAffinity();
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.changeMenu:
+                AlertDialog.Builder window= new AlertDialog.Builder(this);
+
+                final String[] opciones = {"Piano Tradicional", "Piano Infantil de la Selva", "Piano de Instrumentos musicales"};
+
+
+                window.setItems(opciones, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        //here program the activity and toast
+                        if (opciones[item].equals("Piano Tradicional")) {
+                            Intent pianoTradicional = new Intent(AcercaDe.this, Piano_tradicional.class);
+                            startActivity(pianoTradicional);
+                        } else if (opciones[item].equals("Piano Infantil de la Selva")) {
+                            Intent pianoJungle = new Intent(AcercaDe.this, Piano_infantil_selva.class);
+                            startActivity(pianoJungle);
+
+                        } else if (opciones[item].equals("Piano de Instrumentos musicales")) {
+                            Intent pianoMusic = new Intent(AcercaDe.this, piano_Instrumentos.class);
+                            startActivity(pianoMusic);
+                        }
+                    }
+                });
+
+                window.create();
+                window.show();
+                break;
+            case R.id.about:
+                Intent about = new Intent(AcercaDe.this,AcercaDe.class);
+                startActivity(about);
+                break;
+            case R.id.exit:
+                CerrarAplicacion();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
