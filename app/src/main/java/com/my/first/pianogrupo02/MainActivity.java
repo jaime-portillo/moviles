8d7a756caf554f74d33eb158cfd60827594a7845
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-11 02:14:28 -0600

    menu2

--- app/src/main/java/com/my/first/pianogrupo02/MainActivity.java
+++ app/src/main/java/com/my/first/pianogrupo02/MainActivity.java
@@ -2,7 +2,11 @@ package com.my.first.pianogrupo02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimerTask tm = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Piano_tradicional.class);
+                startActivity(intent);
+                finish();
+
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(tm,3000);
     }
 }
